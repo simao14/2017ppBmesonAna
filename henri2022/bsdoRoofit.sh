ae8a9ebdd004e06de33348e1d80dc6e7117c64d7
Legoinha <henrique.legoinha@tecnico.ulisboa.pt>
2023-07-31 17:12:12 +0100

    now should be workig

--- henri2022/bsdoRoofit.sh
+++ henri2022/bsdoRoofit.sh
@@ -28,37 +28,37 @@ OutputFile_BS_BINNED_MULT="ROOTfiles/yields_Bs_binned_Mult.root"
 OutputFile_BS_BINNED_MULT_trk="ROOTfiles/yields_Bs_binned_Mult_trk.root"
 
 if [ $DOANALYSISPbPb_FULL_BS  -eq 1  ]; then
-root -b  -q 'roofitB.C+('\"ntphi\"','1','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$CUTPbPb\"','\"$OutputFile_BS_FULL\"','\"results/Bs/Bpt\"','1','"0"')'
+root -b  -q 'roofitB.C+('\"ntphi\"','1','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$CUTPbPb\"','\"$OutputFile_BS_FULL\"','\"results/Bs/Bpt\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
 
 if [ $DOANALYSISPbPb_BINNED_PT_BS  -eq 1  ]; then
-root -b  -q 'roofitB.C+('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_PT\"','\"results/Bs/Bpt\"','1','"0"')'
+root -b  -q 'roofitB.C+('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_PT\"','\"results/Bs/Bpt\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
 
 if [ $DOANALYSISPbPb_BINNED_PT_BS_TRK -eq 1 ]; then
-root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$cut_trk_tight\"','\"$OutputFile_BS_BINNED_PT_trk\"','\"results/Bs/trk_tight\"','1','"0"')'
+root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"Bpt\"','\"$cut_trk_tight\"','\"$OutputFile_BS_BINNED_PT_trk\"','\"results/Bs/trk_tight\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
 
 if [ $DOANALYSISPbPb_BINNED_Y_BS  -eq 1  ]; then
-root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"By\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_Y\"','\"results/Bs/By\"','1','"0"')'
+root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"By\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_Y\"','\"results/Bs/By\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
 
 if [ $DOANALYSISPbPb_BINNED_Y_BS_TRK  -eq 1  ]; then
-root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"By\"','\"$cut_trk_tight\"','\"$OutputFile_BS_BINNED_Y_trk\"','\"results/Bs/By\"','1','"0"')'
+root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"By\"','\"$cut_trk_tight\"','\"$OutputFile_BS_BINNED_Y_trk\"','\"results/Bs/By\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
 
 if [ $DOANALYSISPbPb_BINNED_MULT_BS  -eq 1  ]; then
-root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"nMult\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_MULT\"','\"results/Bs/nMult\"','1','"0"')'
+root -b  -q 'roofitB.C('\"ntphi\"','0','\"$Data_Bs\"','\"$MC_Bs\"','\"nMult\"','\"$CUTPbPb\"','\"$OutputFile_BS_BINNED_MULT\"','\"results/Bs/nMult\"','1','\"0\"')'
 
 rm roofitB_C.d roofitB_C_ACLiC_dict_rdict.pcm roofitB_C.so
 fi
