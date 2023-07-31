cf723a887c46042a70b98c0278724caedbd94881
Legoinha <henrique.legoinha@tecnico.ulisboa.pt>
2023-07-31 22:53:57 +0100

    variables

--- EffAna/CrossSectionAnaMult.C
+++ EffAna/CrossSectionAnaMult.C
@@ -247,7 +247,7 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 		var_M="p_{T}";
 		var_file="pt";
 		NBins=nptBinsBP;}
-	if (whichvar==0 && meson_n!=0){
+	if (whichvar==0 && meson_n==1){
 		var_M="p_{T}";
 		var_file="pt";
 		NBins=nptBins;}
@@ -387,7 +387,6 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 
 	if(DoTnP == 0) fin1DEff = new TFile(Form("%s/NewEff2DMaps/EffFineNoTnP.root",var_n.Data()));
 	if(DoTnP == 1) fin1DEff = new TFile(Form("%s/NewEff2DMaps/EffFineBDT.root",var_n.Data()));	
-
 
 	fin1DEff->cd();
 
@@ -641,17 +640,16 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 
 
 	
-
 	//TFile * RawYield = new TFile(Form("../../henri2022/ROOTfiles/yields_Bp_binned_%s.root",var_file.Data()));
 	TString fYield = Form("../henri2022/ROOTfiles/yields_%s_binned_%s.root",var_n2.Data(),var_file.Data());
 	TFile * RawYield = new TFile(fYield);
-	RawYield->cd();
-	TH1D * hPt = (TH1D *) RawYield->Get("hPt");
+	TMultiGraph *mg1= (TMultiGraph*) RawYield->Get("TG");
+    TGraphAsymmErrors* Y_RAW = dynamic_cast<TGraphAsymmErrors*>(mg1->GetListOfGraphs()->FindObject("Y_stat"));
 
-	TFile * RawYieldTight;
-	TH1D * hPtTight;
+	TFile * RawYieldTight;   
+	TMultiGraph * hPtTight;
 	RawYieldTight = new TFile(TString(fYield(0, fYield.Length() - 5)) + "_trk.root");
-	hPtTight = (TH1D *) RawYieldTight->Get("hPt");
+	hPtTight = (TH1D *) RawYieldTight->Get("TG")->GetListOfGraphs()->At(0);
 
 	double RawCount;
 	double RawCountErr;
@@ -664,8 +662,8 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 
 	for(int i = 0; i < NBins;i++){
 
-		RawCount = hPt->GetBinContent(i+1);
-		RawCountErr = hPt->GetBinError(i+1);
+		RawCount = Y_RAW->GetBinContent(i+1);
+		RawCountErr = Y_RAW->GetBinError(i+1);
 
 		cout << "RawCount = " << RawCount << "  RawCountErr = " << RawCountErr << " NewEff[i] =   " << NewEff[i] << "  NewEffErr[i] =  " << NewEffErr[i] << endl; 
 
@@ -700,8 +698,8 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 	TH1D * CorrDiffHisTight = (TH1D*) CorrDiffHis->Clone("hPtSigma_tight");
 
 	for(int i = 0; i < NBins;i++){
-		RawCount = hPt->GetBinContent(i+1);
-		RawCountErr = hPt->GetBinError(i+1);
+		RawCount = Y_RAW->GetBinContent(i+1);
+		RawCountErr = Y_RAW->GetBinError(i+1);
 		CorrYieldDiff[i] = (RawCount *  NewEff[i])/(BRchain*2* lumi);
 		CorrYieldDiffErr[i] = TMath::Sqrt((RawCountErr *  NewEff[i]) *(RawCountErr  *  NewEff[i]) + (RawCount *  NewEffErr[i]) * (RawCount  *  NewEffErr[i]))/(BRchain*2* lumi);
 		CorrDiffHis->SetBinContent(i+1,CorrYieldDiff[i]);
@@ -746,8 +744,8 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 
 
 	for(int i = 0; i < NBins;i++){
-		RawCount = hPt->GetBinContent(i+1);
-		RawCountErr = hPt->GetBinError(i+1);
+		RawCount = Y_RAW->GetBinContent(i+1);
+		RawCountErr = Y_RAW->GetBinError(i+1);
 		CorrYieldDiff[i] = (RawCount /  NewEffReal[i])/(BRchain*2* lumi);
 		CorrYieldDiffErr[i] = TMath::Sqrt((RawCountErr /  NewEffReal[i]) *(RawCountErr  /  NewEffReal[i]) + (RawCount /NewEffReal[i] *  NewEffRealErr[i]) * (RawCount/NewEffReal[i]  *  NewEffRealErr[i]))/(BRchain*2* lumi);
 		CorrDiffHisReal->SetBinContent(i+1,CorrYieldDiff[i]);
@@ -871,8 +869,8 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 	double YTemp;
 
 	for(int i = 0; i < NBins;i++){
-		RawCount = hPt->GetBinContent(i+1);
-		RawCountErr = hPt->GetBinError(i+1);
+		RawCount = Y_RAW->GetBinContent(i+1);
+		RawCountErr = Y_RAW->GetBinError(i+1);
 		Eff1D[i] = Eff1DHisvar->GetBinContent(i+1);
 		Eff1DErr[i] = Eff1DHisvar->GetBinError(i+1);
 
@@ -1125,7 +1123,7 @@ void CrossSectionAnaMult(int DoTnP,int whichvar,int meson_n,int usemc=0){
 	InvEff1DHisvar->Write();
 	InvEff1DHisTight->Write();
 	InvEff1DHisLoose->Write();
-	hPt->Write();
+	Y_RAW->Write();
 	foutCorr->Close();
 
 }
